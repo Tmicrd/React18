ffbb7df651cce7f6f073b467915f8af7ecd44883
nancy <liyingxiong23@gmail.com>
2024-07-18 10:10:41 +1000

    styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   const [showAlert, setShowAlert] = useState(false);
+  const items = ["New York", "Tokyo", "Paris", "Sydney", "London"];
+
+  const onSelectItem = (item: string) => {
+    console.log(`Selected item: ${item}`);
+  };
+
   return (
     <>
       {showAlert && <Alert onClose={() => setShowAlert(false)}>Alert</Alert>}
       <Button onClick={() => setShowAlert(true)}>Button</Button>
+      <ListGroup items={items} heading="Cities" onSelectItem={onSelectItem} />
     </>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
+import styled from "styled-components";
 
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
 interface ListGroupProps {
   items: string[];
   heading: string;
@@ -7,16 +21,16 @@ interface ListGroupProps {
 }
 
 function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
+      <List className="list-group">
         {items.map((item, index) => (
-          <li
-            className={`list-group-item ${selectedIndex === index && "active"}`}
+          <ListItem
+            active={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -24,9 +38,9 @@ function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
