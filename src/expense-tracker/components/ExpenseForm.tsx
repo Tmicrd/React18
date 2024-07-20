e1cb88806057f5e07e1975f320732e466d04a42f
nancy <liyingxiong23@gmail.com>
2024-07-20 22:03:47 +1000

    removed unnecessary lines

--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -1,6 +1,5 @@
 // import { z } from "zod";
 import { z } from "zod";
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CATEGORIES } from "../categories";
--- src/expense-tracker/components/ExpenseList.tsx
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -14,45 +14,43 @@ const ExpenseList = ({ expenses, onDelete }: Props) => {
   if (expenses.length === 0) return null;
 
   return (
-    <div>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Category</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense) => (
-            <tr key={expense.id}>
-              <td>{expense.description}</td>
-              <td>{expense.amount}</td>
-              <td>{expense.category}</td>
-              <td>
-                <button
-                  className="btn btn-outline-danger"
-                  onClick={() => onDelete(expense.id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Total</td>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
             <td>
-              ${expenses.reduce((pre, cur) => pre + cur.amount, 0).toFixed(2)}
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
             </td>
-            <td>Total</td>
-            <td>Total</td>
           </tr>
-        </tfoot>
-      </table>
-    </div>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            ${expenses.reduce((pre, cur) => pre + cur.amount, 0).toFixed(2)}
+          </td>
+          <td>Total</td>
+          <td>Total</td>
+        </tr>
+      </tfoot>
+    </table>
   );
 };
 
